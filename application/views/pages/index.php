78e1e9b230e3346caed072a97c6018fbeb6b722a
lxxkvtme <danu.mahendra79@gmail.com>
2021-12-24 14:44:20 +0700

    pesan kamar

---
+++ application/views/pages/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div>
+            <a href="#">Produk</a>
+            <a href="#">Daftar harga</a>
+            <a href="#">Tentang Kami</a>
+            <a href="#">Pesan Kamar</a>
+        </div>
+    </section>
+</body>
+
+</html>
